efc1d6a0694b8cf41e36bc135af7a96534e4d226
Leonard Monteiro <166357785+Leozin2412@users.noreply.github.com>
2025-06-16 20:19:04 -0300

    Update conexao.js

--- src/database/conexao.js
+++ src/database/conexao.js
@@ -1,7 +1,6 @@
 import { createPool } from 'mysql2/promise';
 
-// O código agora lê as variáveis que a Railway fornece.
-// Se essas variáveis não existirem, ele usa valores padrão para o ambiente local.
+
 const conexao = createPool({
   host: process.env.MYSQLHOST || 'localhost',
   user: process.env.MYSQLUSER || 'root',
