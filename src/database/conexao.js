e28d06496a78dd731cbf6871e2cfe87f0f93f248
Leonard Monteiro <leonardommonteiro2004@gmail.com>
2025-06-16 20:17:48 -0300

    att para hospedagem

--- src/database/conexao.js
+++ src/database/conexao.js
@@ -1,17 +1,16 @@
-// Importamos 'createPool' da versão da biblioteca que suporta Promises
 import { createPool } from 'mysql2/promise';
 
-// Usamos 'createPool', mas atribuímos a uma constante chamada 'conexao'
+// O código agora lê as variáveis que a Railway fornece.
+// Se essas variáveis não existirem, ele usa valores padrão para o ambiente local.
 const conexao = createPool({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '',
-  database: 'GreenWaysOFC', 
+  host: process.env.MYSQLHOST || 'localhost',
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || '',
+  database: process.env.MYSQLDATABASE || 'greenwaysofc',
+  port: process.env.MYSQLPORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-// Exportamos a constante 'conexao'
 export default conexao;
