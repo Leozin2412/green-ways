fc6d60df97467024e2b2df8186197fe24420b3cb
Leonard Monteiro <166357785+Leozin2412@users.noreply.github.com>
2025-06-17 14:30:39 -0300

    Update conexao.js

--- src/database/conexao.js
+++ src/database/conexao.js
@@ -1,9 +1,12 @@
 import { createPool } from 'mysql2/promise';
 
-const connectionString = process.env.DATABASE_URL || 'mysql://root:@localhost:3306/GreenWaysOFC';
 
 const conexao = createPool({
-  uri: connectionString,
+  host: process.env.MYSQLHOST || 'localhost',
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || '',
+  database: process.env.MYSQLDATABASE || 'GreenWaysOFC',
+  port: process.env.MYSQLPORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
