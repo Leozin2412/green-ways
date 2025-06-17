de03794a3fcc099516cad6101a9691af46913736
Leonard Monteiro <166357785+Leozin2412@users.noreply.github.com>
2025-06-17 14:03:55 -0300

    Update conexao.js

--- src/database/conexao.js
+++ src/database/conexao.js
@@ -1,10 +1,6 @@
 import { createPool } from 'mysql2/promise';
 
-// O código procura pela variável DATABASE_URL.
-// Se não a encontrar (no seu ambiente local), ele cria uma URL padrão para o seu PC.
 const connectionString = process.env.DATABASE_URL || 'mysql://root:@localhost:3306/GreenWaysOFC';
-
-console.log("[INFO] Usando a abordagem DATABASE_URL para conexão.");
 
 const conexao = createPool({
   uri: connectionString,
