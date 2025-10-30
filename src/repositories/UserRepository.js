303047181a340640e9f78c931835b5d4c2570b6e
Leozin2412 <166357785+Leozin2412@users.noreply.github.com>
2025-10-30 20:12:15 -0300

    +1

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -25,7 +25,7 @@ export default function createUserRepository(prisma) {
     },
 
     async getById(id) {
-      const numericId = parseInt(id, 10);
+      const numericId = id.parseInt(id, 10);
       const user = await prisma.users.findUnique({
         // O ID no schema do Prisma é um Int, então garantimos que seja um número.
         where: { id: numericId }
